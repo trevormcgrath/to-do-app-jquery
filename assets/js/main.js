6eb4ccda22d4de703223bb00c0b19edc40fc63e7
Trevor McGrath <trevormcgrath88@gmail.com>
2016-08-19 20:22:21 -0700

    Added some styles, added notes into the main.js for app objectives.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
             $taskInput = $("#task-input"),
             $taskSubmit = $('#task-submit');
             $todoList = $('#todo-list');
+            $completedList = $('#completed-list')
+
         /* End globals */
 
         $taskInput.submit(function(e) { e.preventDefault(); }); //Stopping the normal form behavior.
@@ -19,15 +21,39 @@ $(document).ready(function() {
         $taskSubmit.click(function(event) {
             var input = $taskInput.find("input");
             var temp = input.val().trim();
-            $todoList.append('<li><input type="checkbox"><label for=""> '+temp+' </label><span>x</span></li>');
+            $todoList.append('<li><span id="complete"><img src="assets/images/checkmark.png" /></span> <span id="the-task">'+temp+'</span> </label><span id="remove"><img src="assets/images/remove.png" /></span></li>');
             //todoTasks.push(input.val().trim());
             input.val("");
         });
 
-        
+/*** Objectives / Plan:
+            
+    Short run:
+        - When the $todoList's <span id="complete"> is clicked, take <span id="the-task">.html() 
+            into a varibale, put into the $completedList.append() below.
 
-        
-        
+        - When the $todoLis's <span id="remove"> is clicked, put into the... $completedList ?
+
+        - When the $completedList's <span id="add"> is clicked, put back into the $todoList and 
+            show a notification
+
+        - When the $completedList's <span id="trash"> is clicked, make an alert "Permanently delete?" 
+            then just .remove()
+
+    Long run:
+        - In the header area, make the 'Todo' and 'Complete' buttons toggle an add/remove class 
+        transition thing to show the different <div id="todo-container"> versus <div id="completed-container">
+
+        - Add styles to make app look good, use min-width and build mobile styles first, then outwards towards desktop
+
+***/
+
+
+/* this will be the HTML <li> item for the COMPLETED list:
+
+    $completedList.append('<li><span id="add"><img src="assets/images/add.png" /></span> <span id="the-task">'+temp+'</span> </label><span id="trash"><img src="assets/images/trash.png" /></span></li>');
+*/
+
     }(jQuery));
 
 });
