b60fc73c149c67616df5c14b97c3c2c3c48627eb
Martin H <martin@taylorpond.com>
2016-08-22 12:51:10 -0700

    updating responsive styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,8 @@ $(document).ready(function() {
             //Buttons
             $todoListBtn = $('#todo-list-btn'),
             $completedListBtn = $('#completed-list-btn'),
-            $listTabBtns = $('header nav button');
+            $listTabBtns = $('header nav button'),
+            animationSpeed = 150;
 
         /* End globals */
 
@@ -70,92 +71,96 @@ $(document).ready(function() {
                 // add 'visible' class
                 $todoList.addClass('visible');
                 // Display Input Section
-                $inputSection.slideDown(150);
+                $inputSection.slideDown(animationSpeed);
                 // Focus on Task Input
                 taskInput.focus();
             } else {
                 // add class to 'completed' tab
                 $completedList.addClass('visible');
                 // Hide Input Section
-                $inputSection.slideUp(150);
+                $inputSection.slideUp(animationSpeed);
 
             }
         });
 
         /***  This moves completed/clicked tasks from the Todo List to the Completed List  ***/
         $('ul#todo-list').on('click', '.testTask', function(event) {
-            
+
             // when the <lable> is clicked inside of the #todo-list, temp becomes the <li> that surrounds it.
             var temp = this.closest('li');
-            
+
             if (temp != "") {
                 //.prepend the #todo-list task to top of the #completed-list page after 1200ms of being clicked.
                 setTimeout(
-                    function(){ 
-                        $('#completed-list').prepend(temp); }, 
-                        1200
+                    function() {
+                        $('#completed-list').prepend(temp);
+                    },
+                    1200
                 );
-                
+
             }
-            
+
         });
 
         /***  This removes tasks that have had their .remove button clicked.  ***/
         $('ul#todo-list').on('click', '.remove', function(event) {
             var temp = this.closest('li');
-            
+
             setTimeout(
-                    function(){ 
-                        temp.remove(); }, 
-                        1200
-                );
+                function() {
+                    temp.remove();
+                },
+                1200
+            );
         });
 
         /*** Basically the opposite of the above for completion clicks, but moves from completed to todo list ***/
         $('ul#completed-list').on('click', '.testTask', function(event) {
             var temp = this.closest('li');
-            
+
             if (temp != "") {
-                
+
                 setTimeout(
-                    function(){ 
-                        $('#todo-list').prepend(temp); }, 
-                        1200
+                    function() {
+                        $('#todo-list').prepend(temp);
+                    },
+                    1200
                 );
-                
+
             }
         });
 
         /***  This removes tasks that have had their .remove button clicked.  ***/
         $('ul#completed-list').on('click', '.remove', function(event) {
             var temp = this.closest('li');
-            
+
             setTimeout(
-                    function(){ 
-                        temp.remove(); }, 
-                        1200
-                );
+                function() {
+                    temp.remove();
+                },
+                1200
+            );
         });
 
         /*** Objectives / Plan:
 
-        	Short run:
-        		- When the $todoList's <span id="complete"> is clicked, take <span id="the-task">.html()
-        			into a varibale, put into the $completedList.append() below.
+            Short run:
+                - When the $todoList's <span id="complete"> is clicked, take <span id="the-task">.html()
+                    into a varibale, put into the $completedList.append() below.
 
-        		- When the $todoLis's <span id="remove"> is clicked, put into the... $completedList ?
+                - When the $todoLis's <span id="remove"> is clicked, put into the... $completedList ?
 
-        		- When the $completedList's <span id="add"> is clicked, put back into the $todoList and
-        			show a notification
+                - When the $completedList's <span id="add"> is clicked, put back into the $todoList and
+                    show a notification
 
-        		- When the $completedList's <span id="trash"> is clicked, make an alert "Permanently delete?"
-        			then just .remove()
+                - When the $completedList's <span id="trash"> is clicked, make an alert "Permanently delete?"
+                    then just .remove()
 
-        	Long run:
-        		- In the header area, make the 'Todo' and 'Complete' buttons toggle an add/remove class
-        		transition thing to show the different <div id="todo-container"> versus <div id="completed-container">
+            Long run:
+                - In the header area, make the 'Todo' and 'Complete' buttons toggle an add/remove class
+                transition thing to show the different <div id="todo-container"> versus <div id="completed-container">
 
-        		- Add styles to make app look good, use min-width and build mobile styles first, then outwards towards desktop
+                - Add styles to make app look good, use min-width and build mobile styles first, then outwards towards desktop
 
         ***/
     }(jQuery));
