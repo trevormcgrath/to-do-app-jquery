ad998550b72ad70f17d4a5b7e4d06f3134dbe5e0
Chyno Deluxe <chynodeluxe@gmail.com>
2016-09-04 00:27:23 -0700

    refactored js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
 
     var todoApp = (function($) {
 
-        /* Global Variables */
         var active = 'active',
             visible = 'visible',
             animationSpeed = 250,
-
+            taskCount = 0,
             //DOM Elements
             $inputSection = $('#input-section'),
             $taskInput = $("#task-input"),
@@ -21,10 +20,7 @@ $(document).ready(function() {
             $todoListBtn = $('#todo-list-btn'),
             $listTabBtns = $('header nav button'),
             //Local Storage
-            storedTasks = JSON.parse(localStorage.getItem('tasks')) || [],
-            taskCount = 0;
-
-        /* End globals */
+            storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
         function getTaskLi(target) {
             //Get task li element
@@ -32,17 +28,19 @@ $(document).ready(function() {
         }
 
         function fetchStoredTasks() {
-
+            //cycle through local storage 'tasks'
             $.each(storedTasks, function(i) {
+                //build task lists
                 createTask(storedTasks[i]);
-            })
+            });
+            //rewrite local storage 'tasks'
             localStorage.setItem('tasks', JSON.stringify(storedTasks));
         }
 
         function createTask(task) {
             var html = "";
             taskCount += 1;
-
+            //assign task id
             task.id = taskCount;
 
             // Build task HTML
@@ -57,10 +55,12 @@ $(document).ready(function() {
             html += '<span class="remove"></span>';
             html += '</li>';
 
-            //Add new task to DOM
+            //if task is complete
             if (!task.complete) {
+                //Add task to todo list
                 $todoList.prepend(html);
             } else {
+                //Add task to completed list
                 $completedList.prepend(html);
             }
         }
@@ -68,69 +68,115 @@ $(document).ready(function() {
         function checkmarkButton(task) {
             //Get task li element
             task = getTaskLi(task.target);
-            if (task !== "") {
-                $(task).slideUp(230);
-                setTimeout(
-                    function() {
-                        if ($todoListBtn.hasClass(active)) {
-                            $completedList.prepend(task);
-                        } else {
-                            $todoList.prepend(task);
-                        }
-                        $(task).show();
-                    }, animationSpeed);
-            }
+            // slide task out of list
+            $(task).slideUp(230);
+            // delay task completion
+            setTimeout(
+                function() {
+                    // if todo list tab is active
+                    if ($todoListBtn.hasClass(active)) {
+                        // send task to completed list
+                        $completedList.prepend(task);
+                    } else {
+                        //send task to todo list
+                        $todoList.prepend(task);
+                    }
+                    // display task
+                    $(task).show();
+                }, animationSpeed);
         }
 
         function removeButton(task) {
             //Get task li element
             task = getTaskLi(task.target);
-            //slide up task
+            // slide task out of list
             $(task).slideUp(230);
             //delay task removal
             setTimeout(
                 function() {
-                    //remove task
+                    // remove task
                     $(task).remove();
-                },
-                animationSpeed);
+                }, animationSpeed);
         }
 
-        //Fetch Todos
+        function getTask($el) {
+            // turn elem to jQuery obj
+            $el = $($el);
+            /* ------------------------------------------
+             * return appropreate task obj
+             * depending on click target
+             * ------------------------------------------ */
+            if ($el.hasClass('taskLabel')) {
+                return {
+                    todo: $el.text(),
+                    id: Number($el.attr('for'))
+                };
+            } else {
+                return {
+                    todo: $el.prev().text(),
+                    id: Number($el.siblings('input').attr('id'))
+                };
+            }
+
+        }
+
+        function taskIsAMatch(task) {
+            // cycle through local storage 'tasks'
+            for (var i = 0; i < storedTasks.length; i++) {
+                // assign current task
+                var storedTask = storedTasks[i];
+                /* ------------------------------------------
+                 * return current task IF
+                 * 1. current task text = stored task text
+                 * 2. current task id = stored task id
+                 * ------------------------------------------ */
+                if (task.todo === storedTask.todo &&
+                    task.id === storedTask.id) {
+                    return storedTask;
+                }
+            }
+        }
+        // Fetch Todos
         fetchStoredTasks();
 
-        //Stopping the normal form behavior.
+        // on submit: prevent default behavior.
         $taskInput.submit(function(e) { e.preventDefault(); });
 
-        /*** $taskSubmit:
-        Add a task from input into the todoTasks array, then clear the input. ***/
+        /* ------------------------------------------
+         * $taskSubmit:
+         * Add a task from input into the
+         * todoTasks array, then clear the input.
+         * ------------------------------------------*/
         $taskSubmit.click(function() {
-
+            // find input feild
             var input = $taskInput.find("input"),
+                // get todo text
                 todo = input.val().trim(),
+                // init task obj
                 task = {};
 
+            // if task input is not empty
             if (todo !== "") {
+                // add todo text to task obj
                 task.todo = todo;
-
+                // push obj to stored tasks array
                 storedTasks.push(task);
                 //Add new task to DOM
                 createTask(task);
-
+                // write tasks json to local storage
                 localStorage.setItem('tasks', JSON.stringify(storedTasks));
             }
-
             //Reset input value
             input.val("").focus();
         });
 
-        /*** $listTabBtns
-        - Add 'active' class to list tab when clicked
-        - Display active task list
-        ***/
+        // Todo list tabs
         $listTabBtns.click(function() {
+            // get active tab
             var $activeTab = $(this),
+                // get task input elm
                 taskInput = $taskInput.find('input');
+
             // remove active class
             $listTabBtns.removeClass(active);
             // remove visible class from both list-containers
@@ -154,59 +200,41 @@ $(document).ready(function() {
             }
         });
 
-        function getTask($el) {
-            $el = $($el);
-
-            if ($el.hasClass('taskLabel')) {
-                return {
-                    todo: $el.text(),
-                    id: Number($el.attr('for'))
-                };
-            } else {
-                return {
-                    todo: $el.prev().text(),
-                    id: Number($el.siblings('input').attr('id'))
-                };
-            }
-
-        }
-
-        function taskIsAMatch(task, storedTask) {
-
-            return (task.todo === storedTask.todo &&
-                task.id === storedTask.id);
-        }
-
+        // on task click
         $taskLists.on('click', '.taskLabel, .remove', function(e) {
+            // stop event bubbling
             e.stopPropagation();
 
+            // target elm
             var $el = $(this),
-                task = getTask($el);
+                // current task
+                task = getTask($el),
+                // find matching task in local storage
+                storedTask = taskIsAMatch(task),
+                // current local storage task index
+                tsk_ind = storedTasks.indexOf(storedTask);
 
+            // if click target is task label or complete btn
             if ($el.hasClass('complete') || $el.hasClass('taskLabel')) {
-
-                for (var i = 0; i < storedTasks.length; i++) {
-                    var storedTask = storedTasks[i];
-                    if (task.todo === storedTask.todo &&
-                        task.id === storedTask.id) {
-                        storedTask.complete = storedTask.complete ? false : true;
-                        console.log(task);
-                        localStorage.setItem('tasks', JSON.stringify(storedTasks));
-                        checkmarkButton(e);
-                    }
-                }
+                /* ------------------------------------------
+                 * if task complete
+                 * -> set storedTask.complete to true
+                 * else
+                 * -> set to false
+                 * ------------------------------------------ */
+                storedTask.complete = storedTask.complete ? false : true;
+                // write 'tasks' to localStorage
+                localStorage.setItem('tasks', JSON.stringify(storedTasks));
+                // complete task
+                checkmarkButton(e);
 
             } else {
-                for (var i = 0; i < storedTasks.length; i++) {
-                    var storedTask = storedTasks[i];
-
-                    if (task.todo === storedTask.todo &&
-                        task.id === storedTask.id) {
-                        storedTasks.splice(i, 1);
-                        localStorage.setItem('tasks', JSON.stringify(storedTasks));
-                        removeButton(e);
-                    }
-                }
+                // remove current task
+                storedTasks.splice(tsk_ind, 1);
+                // write 'tasks' to localStorage
+                localStorage.setItem('tasks', JSON.stringify(storedTasks));
+                // remove task
+                removeButton(e);
             }
         });
     }(jQuery));
